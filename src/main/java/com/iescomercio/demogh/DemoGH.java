ddaf218d270b6de5ee9b5d044e76521a5e5a3f62
jroca <joelrocamolano@gmail.com>
2025-02-19 16:18:09 +0100

    Add demoGH + first commit

---
+++ src/main/java/com/iescomercio/demogh/DemoGH.java
@@ -0,0 +1,12 @@
+package com.iescomercio.demogh;
+
+/**
+ *
+ * @author Joel Roca
+ */
+public class DemoGH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
